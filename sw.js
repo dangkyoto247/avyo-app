6ec04309bd9e768d330c3a42881ae328b4e84e4f
dangkyoto247 <admin@dangkyoto247.com>
2026-09-12 02:30:59 +0700

    s

--- sw.js
+++ sw.js
@@ -3,5 +3,5 @@ self.addEventListener('install', (e) => {
 });
 
 self.addEventListener('fetch', (e) => {
-  // Bỏ qua cache để luôn lấy dữ liệu mới nhất
+  // Bỏ qua cache để luôn lấy code mới nhất, không bị lỗi lưu web cũ
 });
