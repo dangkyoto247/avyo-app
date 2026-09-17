b38cfca0eaef8db1728a13e703b53b37ad363921
dangkyoto247 <admin@dangkyoto247.com>
2026-09-17 21:16:56 +0700

    d

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const TILE_CACHE_NAME = 'map-tiles-v2';
-const STATIC_CACHE_NAME = 'avyo-static-v96';
+const STATIC_CACHE_NAME = 'avyo-static-v97';
 
 const STATIC_ASSETS = [
   'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;800&display=swap',
